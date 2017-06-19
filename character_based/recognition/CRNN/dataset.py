fcf1513ffce0fb0197cde040a7b325adbe386a5e
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2017-06-19 14:22:30 +0700

    Refactor CRNN code

--- character_based/recognition/CRNN/dataset.py
+++ character_based/recognition/CRNN/dataset.py
@@ -1,205 +1,201 @@
 from __future__ import division, print_function, unicode_literals
-import os
 import json
-import sys
 import cv2
 import pickle
 import numpy as np
-seed = 13
-np.random.seed(seed)
 
 from utils import Timer
-from constants import (HEIGHT, WIDTH, MAX_WORD_LENGTH,
-					SAMPLE_PER_WORD, PICKLE_DATA, DATA_FOLDERS,
-					DATA_NAMES, IDX_MAPS, VN_DICT,
-					CHAR_LABEL_MAP)
+from constants import *
+
+
+seed = 13
+np.random.seed(seed)
 
 DATA_FOLDER = DATA_FOLDERS[0]
 DATA_NAME = DATA_NAMES[0]
 
 
-class Dataset():
-	def __init__(self, data_name, idx_map=None):
-		# load dataset from pickle files
-		self.X = None
-		self.Y = None
-		self._load_data(data_name)
+class Dataset:
+    def __init__(self, data_name, idx_map=None):
+        # load dataset from pickle files
+        self.X = None
+        self.Y = None
+        self._load_data(data_name)
 
-		self.train_idx = []
-		self.val_idx = []
-		self.test_idx = []
+        if idx_map:
+            # read index map from file
+            self._read_train_val_test_set(idx_map)
 
-		if idx_map:
-			# read dataset from files
-			self._read_train_val_test_set(idx_map)
+            self.train_idx = np.array(self.train_idx, dtype='int')
+            self.val_idx = np.array(self.val_idx, dtype='int')
+            self.test_idx = np.array(self.test_idx, dtype='int')
 
-			self.train_idx = np.array(self.train_idx, dtype='int')
-			self.val_idx = np.array(self.val_idx, dtype='int')
-			self.test_idx = np.array(self.test_idx, dtype='int')
+    def get_dataset(self):
+        return self.X, self.Y
 
-	def get_dataset(self):
-		return self.X, self.Y
+    def get_train_dataset(self):
+        return self._get_dataset(self.train_idx)
 
-	def get_train_dataset(self):
-		return self._get_dataset(self.train_idx)
+    def get_val_dataset(self):
+        return self._get_dataset(self.val_idx)
 
-	def get_val_dataset(self):
-		return self._get_dataset(self.val_idx)
+    def get_test_dataset(self):
+        return self._get_dataset(self.test_idx)
 
-	def get_test_dataset(self):
-		return self._get_dataset(self.test_idx)
+    def create_new_train_val_test_set(self, ratio=(0.8, 0.8)):
+        train_idx = []
+        val_idx = []
+        test_idx = []
 
-	def create_new_train_val_test_set(self, ratio=(0.8, 0.8)):
-		temp = []
-		for i in range(self.X.shape[0]):
-			r = np.random.random()
-			if r < ratio[0]:
-				temp.append(i)
-			else:
-				self.test_idx.append(i)
+        temp = []
+        for i in range(self.X.shape[0]):
+            r = np.random.random()
+            if r < ratio[0]:
+                temp.append(i)
+            else:
+                test_idx.append(i)
 
-		for i in temp:
-			r = np.random.random()
-			if r < ratio[1]:
-				self.train_idx.append(i)
-			else:
-				self.val_idx.append(i)
+        for i in temp:
+            r = np.random.random()
+            if r < ratio[1]:
+                train_idx.append(i)
+            else:
+                val_idx.append(i)
 
-		np.random.shuffle(self.train_idx)
-		np.random.shuffle(self.val_idx)
-		np.random.shuffle(self.test_idx)
+        np.random.shuffle(train_idx)
+        np.random.shuffle(val_idx)
+        np.random.shuffle(test_idx)
 
-		# save their indexes to files
-		with open(IDX_MAPS + "map_{}".format(self.X.shape[0]), "w") as f:
-			f.write(json.dumps({
-				"train_idx": self.train_idx,
-				"val_idx": self.val_idx,
-				"test_idx": self.test_idx
-			}))
+        # save their indexes to files
+        with open(IDX_MAPS + "map_{}".format(self.X.shape[0]), "w") as f:
+            f.write(json.dumps({
+                "train_idx": train_idx,
+                "val_idx": val_idx,
+                "test_idx": test_idx
+            }))
 
-	def _get_dataset(self, idx_list):
-		X = self.X[idx_list]
-		Y = self.Y[idx_list]
-		return X, Y
+    def _get_dataset(self, idx_list):
+        X = self.X[idx_list]
+        Y = self.Y[idx_list]
+        return X, Y
 
-	def _load_data(self, data_name):
-		with open(data_name, "rb") as f:
-			self.X = pickle.load(f)
-			self.Y = pickle.load(f)
+    def _load_data(self, data_name):
+        with open(data_name, "rb") as f:
+            self.X = pickle.load(f)
+            self.Y = pickle.load(f)
 
-	def _read_train_val_test_set(self, file_name):
-		with open(file_name, "r") as f:
-			obj = json.loads(f.read())
-		self.train_idx = obj["train_idx"]
-		self.val_idx = obj["val_idx"]
-		self.test_idx = obj["test_idx"]
-
+    def _read_train_val_test_set(self, file_name):
+        with open(file_name, "r") as f:
+            obj = json.loads(f.read())
+        self.train_idx = obj["train_idx"]
+        self.val_idx = obj["val_idx"]
+        self.test_idx = obj["test_idx"]
 
 
 def pickle_data(data, data_name):
-	print("Saving...")
-	with open(data_name, "wb") as f:
-		for i in data:
-			pickle.dump(i, f, protocol=pickle.HIGHEST_PROTOCOL)
+    print("Saving...")
+    with open(data_name, "wb") as f:
+        for i in data:
+            pickle.dump(i, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def pickle_character_list_data(all_labels):
-	"""
-	pickle images of words
-	each example is a list of characters
-	"""
-	all_labels.sort()
+    """
+    pickle images of words
+    each example is a list of characters
+    """
 
-	with open(VN_DICT, 'r', encoding='utf8') as f:
-		word_list = f.read().split()
+    all_labels.sort()
 
-	with open(CHAR_LABEL_MAP, 'r', encoding='utf8') as f:
-		char_label_map = json.loads(f.read(), encoding='utf8')
+    with open(VN_DICT, 'r', encoding='utf8') as f:
+        word_list = f.read().split()
 
-	# Create lowercase, uppercase, camel case for each word
-	word_list_all_forms = []
-	for w in word_list:
-		word_list_all_forms.append(w.lower())
-		word_list_all_forms.append(w.upper())
-		word_list_all_forms.append(w.title())
-		if w not in word_list_all_forms:
-			word_list_all_forms.append(w)
+    with open(CHAR_LABEL_MAP, 'r', encoding='utf8') as f:
+        char_label_map = json.loads(f.read(), encoding='utf8')
 
-	print("Total words: ", len(word_list_all_forms))
+    # Create lowercase, uppercase, camel case for each word
+    word_list_all_forms = []
+    for w in word_list:
+        word_list_all_forms.append(w.lower())
+        word_list_all_forms.append(w.upper())
+        word_list_all_forms.append(w.title())
+        if w not in word_list_all_forms:
+            word_list_all_forms.append(w)
 
-	X = np.zeros((0, MAX_WORD_LENGTH, HEIGHT, WIDTH), dtype='float32')
-	Y = np.zeros((0, MAX_WORD_LENGTH, len(all_labels) + 1), dtype='int')
+    print("Total words: ", len(word_list_all_forms))
 
-	break_points = np.linspace(1, len(word_list_all_forms), 15, endpoint=True, dtype='int')[1:] - 1
+    X = np.zeros((0, MAX_WORD_LENGTH, HEIGHT, WIDTH), dtype='float32')
+    Y = np.zeros((0, MAX_WORD_LENGTH, len(all_labels) + 1), dtype='int')
 
-	count = -1
-	for w in word_list_all_forms:
-		count += 1
+    break_points = np.linspace(1, len(word_list_all_forms), 15, endpoint=True, dtype='int')[1:] - 1
 
-		if len(w) > MAX_WORD_LENGTH:  # if word is longer than MAX_WORD_LENGTH characters => skip
-			continue
+    count = -1
+    for w in word_list_all_forms:
+        count += 1
 
-		all_imgs = []
-		labels = np.zeros((SAMPLE_PER_WORD, MAX_WORD_LENGTH, len(all_labels) + 1))
+        if len(w) > MAX_WORD_LENGTH:  # if word is longer than MAX_WORD_LENGTH characters => skip
+            continue
 
-		for i in range(len(w)):
-			char = w[i]
-			l = char_label_map[char]  # label of character ith
+        all_imgs = []
+        labels = np.zeros((SAMPLE_PER_WORD, MAX_WORD_LENGTH, len(all_labels) + 1))
 
-			img_names = np.random.choice(os.listdir(DATA_FOLDER + l), SAMPLE_PER_WORD, replace=False)
-			img_srcs = [DATA_FOLDER + "{}/{}".format(l, n) for n in img_names]
-			imgs = [cv2.imread(src, cv2.IMREAD_GRAYSCALE) for src in img_srcs]
-			imgs = np.array(imgs) / 255.0
+        for i in range(len(w)):
+            char = w[i]
+            l = char_label_map[char]  # label of character ith
 
-			all_imgs.append(imgs)
-			labels[:, i, all_labels.index(l)] = 1
+            img_names = np.random.choice(os.listdir(DATA_FOLDER + l), SAMPLE_PER_WORD, replace=False)
+            img_srcs = [DATA_FOLDER + "{}/{}".format(l, n) for n in img_names]
+            imgs = [cv2.imread(src, cv2.IMREAD_GRAYSCALE) for src in img_srcs]
+            imgs = np.array(imgs) / 255.0
 
-		for i in range(len(w), MAX_WORD_LENGTH):
-			all_imgs.append(np.zeros((SAMPLE_PER_WORD, HEIGHT, WIDTH), dtype='float32'))
-			labels[:, i, len(all_labels)] = 1
+            all_imgs.append(imgs)
+            labels[:, i, all_labels.index(l)] = 1
 
-		all_imgs = np.transpose(np.array(all_imgs), (1, 0, 2, 3))  # (SAMPLE_PER_WORD, MAX_WORD_LENGTH, HEIGHT, WIDTH)
+        for i in range(len(w), MAX_WORD_LENGTH):
+            all_imgs.append(np.zeros((SAMPLE_PER_WORD, HEIGHT, WIDTH), dtype='float32'))
+            labels[:, i, len(all_labels)] = 1
 
-		X = np.vstack((X, all_imgs))
-		Y = np.vstack((Y, labels))
+        all_imgs = np.transpose(np.array(all_imgs), (1, 0, 2, 3))  # (SAMPLE_PER_WORD, MAX_WORD_LENGTH, HEIGHT, WIDTH)
 
-		if count in break_points:
-			print("Saving up to {}...".format(count + 1))
-			X = X.reshape(X.shape[0], MAX_WORD_LENGTH, HEIGHT, WIDTH, 1)
-			
-			pickle_data([X, Y], DATA_NAME + '_temp_' + str(np.where(break_points==count)[0][0]))
-			
-			X = np.zeros((0, MAX_WORD_LENGTH, HEIGHT, WIDTH), dtype='float32')
-			Y = np.zeros((0, MAX_WORD_LENGTH, len(all_labels) + 1), dtype='int')
+        X = np.vstack((X, all_imgs))
+        Y = np.vstack((Y, labels))
 
+        if count in break_points:
+            print("Saving up to {}...".format(count + 1))
+            X = X.reshape(X.shape[0], MAX_WORD_LENGTH, HEIGHT, WIDTH, 1)
 
-	print("Merging to single file...")
-	X = np.zeros((0, MAX_WORD_LENGTH, HEIGHT, WIDTH, 1), dtype='float32')
-	Y = np.zeros((0, MAX_WORD_LENGTH, len(all_labels) + 1), dtype='int')
+            pickle_data([X, Y], DATA_NAME + '_temp_' + str(np.where(break_points == count)[0][0]))
 
-	for i in range(break_points.shape[0]):
-		filename = DATA_NAME + '_temp_' + str(i)
-		with open(filename, 'rb') as f:
-			X = np.vstack((X, pickle.load(f)))
-			Y = np.vstack((Y, pickle.load(f)))
-		os.remove(filename)
+            X = np.zeros((0, MAX_WORD_LENGTH, HEIGHT, WIDTH), dtype='float32')
+            Y = np.zeros((0, MAX_WORD_LENGTH, len(all_labels) + 1), dtype='int')
 
-	pickle_data([X, Y], DATA_NAME)
+    print("Merging to single file...")
+    X = np.zeros((0, MAX_WORD_LENGTH, HEIGHT, WIDTH, 1), dtype='float32')
+    Y = np.zeros((0, MAX_WORD_LENGTH, len(all_labels) + 1), dtype='int')
+
+    for i in range(break_points.shape[0]):
+        filename = DATA_NAME + '_temp_' + str(i)
+        with open(filename, 'rb') as f:
+            X = np.vstack((X, pickle.load(f)))
+            Y = np.vstack((Y, pickle.load(f)))
+        os.remove(filename)
+
+    pickle_data([X, Y], DATA_NAME)
 
 
 if __name__ == "__main__":
-	t = Timer()
-	t.start("Processing...")
+    t = Timer()
+    t.start("Processing...")
 
-	if not os.path.exists(PICKLE_DATA):
-		os.makedirs(PICKLE_DATA)
+    if not os.path.exists(PICKLE_DATA):
+        os.makedirs(PICKLE_DATA)
 
-	all_labels = os.listdir(DATA_FOLDER)
+    all_labels = os.listdir(DATA_FOLDER)
 
-	pickle_character_list_data(all_labels)
+    pickle_character_list_data(all_labels)
 
-	# create idx map
-	d = Dataset(DATA_NAME)
-	d.create_new_train_val_test_set()
+    # create idx map
+    d = Dataset(DATA_NAME)
+    d.create_new_train_val_test_set()
 
-	t.stop()
+    t.stop()
--- character_based/recognition/CRNN/models.py
+++ character_based/recognition/CRNN/models.py
@@ -1,8 +1,5 @@
 from __future__ import division, print_function, unicode_literals
-import sys
 import numpy as np
-seed = 13
-np.random.seed(seed)
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
@@ -13,134 +10,141 @@ from keras.layers.wrappers import TimeDistributed, Bidirectional
 from utils import Timer
 from dataset import Dataset
 import model_handler
-from constants import (HEIGHT, WIDTH, MAX_WORD_LENGTH,
-					DATA_NAMES, MAP_NAMES, MODELS,
-					WEIGHT_NAMES)
+from constants import *
 
-DATA_NAME = DATA_NAMES[1]
+seed = 13
+np.random.seed(seed)
+
+DATA_NAME = DATA_NAMES[0]
 MAP_NAME = MAP_NAMES[1]
-MODEL = MODELS[4]
-WEIGHT_NAME = WEIGHT_NAMES[4]
+MODEL = MODEL_NAMES[4]
+WEIGHT_NAME = WEIGHT_NAMES[5]
 
 
-class NN():
-	def load_data(self):
-		d = Dataset(self.data_name, self.idx_map)
-		self.X_train, self.Y_train = d.get_train_dataset()
-		self.X_val, self.Y_val = d.get_val_dataset()
+class NN:
+    def load_data(self):
+        d = Dataset(self.data_name, self.idx_map)
+        self.X_train, self.Y_train = d.get_train_dataset()
+        self.X_val, self.Y_val = d.get_val_dataset()
 
-	def train(self, epochs):
-		initial_epoch = 0
-		while True:
-			self.model.fit(self.X_train, self.Y_train,
-				  batch_size=self.batch_size,
-				  epochs=epochs,
-				  initial_epoch=initial_epoch,
-				  validation_data=(self.X_val, self.Y_val))
-			initial_epoch = epochs
-			epochs = input("More? ")
-			if not epochs:
-				break
-			else:
-				epochs = int(epochs) + initial_epoch
+    def train(self, epochs):
+        initial_epoch = 0
+        while True:
+            self.model.fit(self.X_train, self.Y_train,
+                           batch_size=self.batch_size,
+                           epochs=epochs,
+                           initial_epoch=initial_epoch,
+                           validation_data=(self.X_val, self.Y_val))
 
-	def save_model(self):
-		name = input("Save model? [Y/n]:  ")
-		if not name:
-			model_handler.save_model(self.model, self.model_name)
-		elif name != 'n':
-			model_handler.save_model(self.model, name)
+            if IS_SERVER:
+                break
 
-		name = input("Save weights? [Y/n]: ")
-		if not name:
-			model_handler.save_weights(self.model, self.weight_name)
-		elif name != 'n':
-			model_handler.save_weights(self.model, name)
+            initial_epoch = epochs
+            epochs = input("More? ")
+            if not epochs:
+                break
+            else:
+                epochs = int(epochs) + initial_epoch
+
+    def save_model(self):
+        if IS_SERVER:
+            model_handler.save_model(self.model, self.model_name)
+            model_handler.save_weights(self.model, self.weight_name)
+        else:
+            name = input("Save model? [Y/n]:  ")
+            if not name:
+                model_handler.save_model(self.model, self.model_name)
+            elif name != 'n':
+                model_handler.save_model(self.model, TRAINED_MODELS + name)
+
+            name = input("Save weights? [Y/n]: ")
+            if not name:
+                model_handler.save_weights(self.model, self.weight_name)
+            elif name != 'n':
+                model_handler.save_weights(self.model, TRAINED_MODELS + name)
 
 
 class CRNN(NN):
-	def __init__(self):
-		self.model = None
-		self.batch_size = 128
-		self.data_name = DATA_NAME
-		self.idx_map = MAP_NAME
-		self.model_name = MODEL
-		self.weight_name = WEIGHT_NAME
+    def __init__(self):
+        self.model = None
+        self.batch_size = 128
+        self.data_name = DATA_NAME
+        self.idx_map = MAP_NAME
+        self.model_name = MODEL
+        self.weight_name = WEIGHT_NAME
 
-		self.num_of_class = None
-		self.height = HEIGHT
-		self.width = WIDTH
-		self.word_length = MAX_WORD_LENGTH
+        self.num_of_class = None
+        self.height = HEIGHT
+        self.width = WIDTH
+        self.word_length = MAX_WORD_LENGTH
 
-	def build(self):
-		self.model = Sequential()
-		self.model.add(TimeDistributed(
-			Conv2D(filters=16,
-				kernel_size=(5, 5),
-				padding='same',
-				input_shape=(self.height, self.width, 1),
-				activation='relu'),
-			input_shape=(self.word_length, self.height, self.width, 1)
-		))
-		self.model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
+    def build(self):
+        self.model = Sequential()
+        self.model.add(TimeDistributed(
+            Conv2D(filters=16,
+                   kernel_size=(5, 5),
+                   padding='same',
+                   input_shape=(self.height, self.width, 1),
+                   activation='relu'),
+            input_shape=(self.word_length, self.height, self.width, 1)
+        ))
+        self.model.add(TimeDistributed(MaxPooling2D(pool_size=(4, 4))))
 
-		self.model.add(TimeDistributed(
-			Conv2D(filters=32,
-				kernel_size=(3, 3),
-				padding='same',
-				input_shape=(self.height, self.width, 1),
-				activation='relu'),
-			input_shape=(self.word_length, self.height, self.width, 1)
-		))
-		self.model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
+        self.model.add(TimeDistributed(
+            Conv2D(filters=32,
+                   kernel_size=(3, 3),
+                   padding='same',
+                   input_shape=(self.height, self.width, 1),
+                   activation='relu'),
+            input_shape=(self.word_length, self.height, self.width, 1)
+        ))
+        self.model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
 
-		self.model.add(TimeDistributed(Flatten()))
+        self.model.add(TimeDistributed(Flatten()))
 
-		self.model.add(Bidirectional(
-			GRU(units=512, return_sequences=True,
-				activation='relu', dropout=0.5),
-			merge_mode='sum'
-		))
-		self.model.add(Bidirectional(
-			GRU(units=512, return_sequences=True,
-			activation='relu', dropout=0.5),
-			merge_mode='sum'
-		))
+        self.model.add(Bidirectional(
+            GRU(units=256, return_sequences=True,
+                activation='relu', dropout=0.5),
+            merge_mode='concat'
+        ))
+        self.model.add(Bidirectional(
+            GRU(units=256, return_sequences=True,
+                activation='relu', dropout=0.5),
+            merge_mode='sum'
+        ))
 
-		self.model.add(TimeDistributed(
-			Dense(self.num_of_class, activation='softmax'),
-			input_shape=(self.word_length, 512)
-		))
-		self.model.summary()
-		input()
+        self.model.add(TimeDistributed(
+            Dense(self.num_of_class, activation='softmax'),
+            input_shape=(self.word_length, 256)
+        ))
 
-		model_handler.compile_model(self.model)
+        model_handler.compile_model(self.model)
 
-	def run(self, build=True):
-		timer = Timer()
+    def run(self, build=True):
+        timer = Timer()
 
-		timer.start("Loading data")
-		self.load_data()
-		self.num_of_class = self.Y_train.shape[2]
-		timer.stop()
-		
-		if build:
-			timer.start("Building model...")
-			self.build()
-			timer.stop()
+        timer.start("Loading data")
+        self.load_data()
+        self.num_of_class = self.Y_train.shape[2]
+        timer.stop()
 
-		timer.start("Training model...")
-		self.train(10)
-		timer.stop()
+        if build:
+            timer.start("Building model...")
+            self.build()
+            timer.stop()
 
-		self.save_model()
+        timer.start("Training model...")
+        self.train(10)
+        timer.stop()
+
+        self.save_model()
 
 
 if __name__ == "__main__":
-	m = CRNN()
+    m = CRNN()
 
-	m.model = model_handler.load_model(MODELS[3])
-	model_handler.compile_model(m.model)
-	m.run(build=False)
+    # m.model = model_handler.load_model(MODEL_NAMES[4])
+    # model_handler.compile_model(m.model)
+    # m.run(build=False)
 
-	# m.run()
+    m.run()
